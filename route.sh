c473b23cafd17c16e90427c98d72db5d0734a3b9
Qin Yang <45936608+QinYang79@users.noreply.github.com>
2024-12-12 17:22:03 +0800

    Update route.sh

--- route.sh
+++ route.sh
@@ -7,15 +7,14 @@ do
 for flags in 0_0_0_0 1_1_1_1
 do
 
-data_path=/disk2/qinyang/SFT/LLama_factory_qy/A_qy/data/dataset
+data_path=your data_path
 mode=dev
-output_path=/disk2/qinyang/SFT/LLama_factory_qy/ROUTE-SQL/output_new/
+output_path=your output_path
 gpus=1
 
-batch_size=16
-# /disk1/chenchao/Models/nl2sql-qwen2.5-14b-sft
-LLM_model=/disk2/qinyang/SFT/LLama_factory_qy/pre_model/rebuttal/full_Llama-3-8B-Instruct_MSFT_clean_1W_epoch2_1e-5_4x16
-tag=llama3_8b_MSFT_clean  # if online in tag, customize your online LLM
+batch_size=32
+LLM_model=your sft model path
+tag=llama3_8b_MSFT  # if online in tag, customize your online LLM
 # for sft/msft LLM 
 CUDA_VISIBLE_DEVICES=0 python route.py --data_path $data_path \
     --dataset $dataset \
@@ -28,11 +27,10 @@ CUDA_VISIBLE_DEVICES=0 python route.py --data_path $data_path \
     --flags $flags \
     --eval_sft 1\ 
 
-
-batch_size=16
-LLM_model=/disk2/qinyang/weights/LLama3
-tag=llama3_8b
-# for sft/msft LLM
+batch_size=32
+LLM_model=your instruct model path
+tag=llama3_8b_instruct
+# for LLM w/o sft/msft
 CUDA_VISIBLE_DEVICES=0 python route.py --data_path $data_path \
     --dataset $dataset \
     --output_path $output_path \
@@ -44,11 +42,9 @@ CUDA_VISIBLE_DEVICES=0 python route.py --data_path $data_path \
     --flags $flags \
     --eval_sft 0\ 
 
-
-
 batch_size=16
-LLM_model=/disk1/chenchao/Models/nl2sql-qwen2.5-14b-sft
-tag=qwen2.5_14b_MSFT_clean  # if online in tag, customize your online LLM
+LLM_model=qwen2.5-14b-msft
+tag=qwen2.5_14b_MSFT  # if online in tag, customize your online LLM
 # for sft/msft LLM 
 CUDA_VISIBLE_DEVICES=0 python route.py --data_path $data_path \
     --dataset $dataset \
@@ -60,7 +56,6 @@ CUDA_VISIBLE_DEVICES=0 python route.py --data_path $data_path \
     --gpus $gpus\
     --flags $flags \
     --eval_sft 1\ 
-    
 done
 done
 
